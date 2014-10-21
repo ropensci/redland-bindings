32dd8f6d00772b52e49074d1ca16eaf1aa7e878f
Matt Jones <gitcode@magisa.org>
2014-10-21 01:00:51 -0800

    Rearranged files into standard R package layout.
    Modified swig.sh to produce files in the correct locations.  Still need to switch back to a Makefile based approach, and resolve issues with the need for a custom Redland.i interface file.

--- R/swig.sh
+++ R/swig.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-declare srcdir = "."
-declare SWIG_OPTS = "-I$(srcdir) -I$(srcdir)/.. -debug-typemap"
+#declare SWIG_OPTS = "-I$(srcdir) -I$(srcdir)/.."
 #swig -v -r $(SWIG_OPTS) -o Redland_wrap.c /usr/share/redland/Redland.i
-swig -v -r -o Redland_wrap.c Redland.i 
+swig -v -r -namespace -outdir redland/R -o redland/src/Redland_wrap.c Redland.i 
+mv redland/R/NAMESPACE redland
+cd redland/src
 R CMD SHLIB Redland_wrap.c

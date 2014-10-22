26d9813eee38878ebfecd4b21ae6a7e1d601ef16
Matt Jones <gitcode@magisa.org>
2014-10-21 22:00:40 -0800

    Modify swig.sh to append to NAMESPACE.

--- R/swig.sh
+++ R/swig.sh
@@ -2,6 +2,7 @@
 #declare SWIG_OPTS = "-I$(srcdir) -I$(srcdir)/.."
 #swig -v -r $(SWIG_OPTS) -o Redland_wrap.c /usr/share/redland/Redland.i
 swig -v -r -namespace -outdir redland/R -o redland/src/Redland_wrap.c Redland.i 
-mv redland/R/NAMESPACE redland
+cat redland/R/NAMESPACE >> redland/NAMESPACE
+rm redland/R/NAMESPACE
 cd redland/src
 R CMD SHLIB Redland_wrap.c

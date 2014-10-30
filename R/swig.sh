30048a7661b0f7ac80b458758ed4a2d5b070a907
Matt Jones <gitcode@magisa.org>
2014-10-29 18:11:00 -0800

    Regenerate NAMESPACE from template on SWIG runs.

--- R/swig.sh
+++ R/swig.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #declare SWIG_OPTS = "-I$(srcdir) -I$(srcdir)/.."
 #swig -v -r $(SWIG_OPTS) -o Redland_wrap.c /usr/share/redland/Redland.i
-swig -v -r -namespace -outdir redland/R -o redland/src/redland.c Redland.i 
+cp NAMESPACE.in redland/NAMESPACE
+swig -v -Wall -r -namespace -outdir redland/R -o redland/src/redland.c Redland.i 
 cat redland/R/NAMESPACE >> redland/NAMESPACE
 rm redland/R/NAMESPACE
 cd redland/src

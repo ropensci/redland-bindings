30289e510829a0e9ea16e292a731a284b9f3c8df
Matt Jones <gitcode@magisa.org>
2014-10-31 01:45:36 -0800

    Enable the ability to detect unbound nodes.
    Unbound nodes are returned with an externalptr set to a null value, which we can now detect with the is.null.externalptr() function.  Added tests showing how to check for unbound nodes.
    
    Closes #6.

--- R/redland/inst/tests/test.redland_base.R
+++ R/redland/inst/tests/test.redland_base.R
@@ -15,6 +15,7 @@ test_that("librdf basic functions", {
     parser <- librdf_new_parser(world,'rdfxml','application/rdf+xml',NULL)
     expect_that(class(parser), matches("_p_librdf_parser_s"))
     uri <- librdf_new_uri(world,'file:../../../../data/dc.rdf')
+    #uri <- librdf_new_uri(world,'file:../../data/dc.rdf')
     expect_that(class(uri), matches("_p_librdf_uri_s"))
     rv <- librdf_parser_parse_into_model(parser,uri,uri,model)
     expect_that(rv, equals(0))
@@ -26,33 +27,31 @@ test_that("librdf basic functions", {
     expect_that(results, not(is_null()))
     expect_that(class(results), matches("_p_librdf_query_results"))
     
+    # Convert the whole sparql result to a string and check its value
     qstr <- librdf_query_results_to_string(results, NULL, NULL)
     expect_that(qstr, matches("http://www.dajobe.org/"))
     expect_that(qstr, matches("Beckett"))
-    
-#     while (!is.null(results) && librdf_query_results_finished(results) == 0) {
-#         num_nodes <- librdf_query_results_get_bindings_count(results)
-#         print(paste("result ", ": {", sep=""))
-#         
-#         for (i in 1:num_nodes-1) {
-#             print(i)
-#             binding_name <- librdf_query_results_get_binding_name(results, i)
-#             val = librdf_query_results_get_binding_value(results, i)
-#             expect_that(class(val), matches("_p_librdf_node_s"))
-#             if (!is.null(val)) {
-#                 nval <- librdf_node_to_string(val)
-#             } else {
-#                 nval = "(unbound)"
-#             }
-#             
-#             print(paste(binding_name, "=", nval, sep=""))
-#         }
-#         
-#         print("}")
-#         rc <- librdf_query_results_next(results)
-#         print(rc)
-#         rc <- librdf_query_results_finished(results)
-#         print(rc)
-#     }
 
+    # Re-execute the query, and gather each bound node and check its value
+    results <- librdf_model_query_execute(model, query);
+    while (!is.null(results) && librdf_query_results_finished(results) == 0) {
+        num_nodes <- librdf_query_results_get_bindings_count(results)
+        for (i in 1:num_nodes-1) {
+            binding_name <- librdf_query_results_get_binding_name(results, i)
+            val = librdf_query_results_get_binding_value(results, i)
+            expect_that(class(val), matches("_p_librdf_node_s"))
+            if (!is.null.externalptr(val@ref)) {
+                nval <- librdf_node_to_string(val)
+                expect_that(class(nval), matches("character"))
+                expect_that(nval, matches("[a-z]"))
+            } else {
+                nval = "(unbound)"
+            }
+            if (i == 2) {
+                expect_that(nval, matches("(unbound)"))
+            }
+            #print(paste(binding_name, "=", nval, sep=""))
+        }
+        rc <- librdf_query_results_next(results)
+    }
 })

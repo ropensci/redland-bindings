ea347bbc1c2926cfde10ee898e87a8142cd4d722
Matt Jones <gitcode@magisa.org>
2014-12-05 11:58:20 -0900

    Add test of Storage with default params.

--- R/redland/inst/tests/test.Storage.R
+++ R/redland/inst/tests/test.Storage.R
@@ -7,14 +7,6 @@ test_that("Storage constructor", {
     world <- new("World")
     expect_that(world, not(is_null()))
     
-    # Test creating Subject, predicate, and object Nodes
-    subject <- new("Node", world, literal="subject")
-    expect_that(class(subject@librdf_node), matches("_p_librdf_node_s"))
-    predicate <- new("Node", world, literal="subject")
-    expect_that(class(predicate@librdf_node), matches("_p_librdf_node_s"))
-    object <- new("Node", world, literal="subject")
-    expect_that(class(object@librdf_node), matches("_p_librdf_node_s"))
-    
     # Test creating the Storage system
     storage <- new("Storage", world, "hashes", name="", options="hash-type='memory'")
     expect_that(storage, not(is_null()))
@@ -23,4 +15,9 @@ test_that("Storage constructor", {
     # Test that storage creation fails if world is not provided or is null
     err <- try(storage <- new("Storage", world=NULL, "hashes", name="", options="hash-type='memory'"), silent=TRUE)
     expect_that(class(err), matches("try-error"))
+    
+    # Test that storage creation succeeds even if type and related options are not provided (use defaults)
+    err <- try(storage <- new("Storage", world=world), silent=TRUE)
+    expect_that(class(err), matches("Storage"))
+    
 })

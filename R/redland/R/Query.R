b9022f97f7aec4e29a803a13d48b202ef0c7bdb2
Peter Slaughter <slaughter@nceas.ucsb.edu>
2015-03-10 11:06:44 -0700

    updated Query.R, added QueryResults.R, test.Query.R, documentation

--- R/redland/R/Query.R
+++ R/redland/R/Query.R
@@ -16,63 +16,92 @@
 #   limitations under the License.
 #
 
-setClass('_p_librdf_query', contains = 'ExternalReference')
-
-#' A Redland Query, used to initialize the Redland RDF library.
-#' @slot query A redland query object
-#' @author Scott Chamberlain
+#' A Query object is used to query an RDF model
+#' @slot librdf_query A redland query object
+#' @author Scott Chamberlain, Peter Slaughter
 #' @rdname Query-class
 #' @include redland.R
 #' @keywords classes
 #' @exportClass Query
 #' @examples
 #' \dontrun{
-#' world <- new("World")
-#' query <- new("Query", querystring="foo bar")
+#' queryString <- 'PREFIX prov: <http://www.w3.org/ns/prov#> SELECT ?a ?c WHERE { ?a prov:Agent ?c . }'
+#' query <- new("Query", world, queryString, base_uri=NULL, query_language="sparql", query_uri=NULL)
 #' }
 setClass("Query", slots = c(librdf_query = "_p_librdf_query"))
 
 #' Initialize the Query object.
+#' @param .Object the Query object
+#' @param world a World object
+#' @param querystring a query string for the language specified in 'query_language'
+#' @param base_uri a URI to prepend to relative URI in the document
+#' @param query_language the query language to execute the querystring with
+#' @param query_uri a URI to prepend to terms in the query
 #' @return the Query object
 #' @export
 setMethod("initialize",
           signature = "Query",
           definition = function(.Object, world, querystring, base_uri=NULL, query_language="sparql", query_uri=NULL) {
-#   .Object@librdf_world <- librdf_new_query();
-#   librdf_world_open(.Object@librdf_world)
-#   return(.Object)
 
-  # self._query = None
   stopifnot(!is.null(querystring))
-
-  # if not PY3 and isinstance(querystring, unicode):
-  #    querystring = querystring.encode('utf-8')
-  # Encoding(querystring) <- 'UTF-8' # not sure how to do this correctly, what is PY3?
-
-  ruri <- if( !is.null(query_uri) ) query_uri$`_reduri` else NULL
-  rbase_uri <- if( !is.null(base_uri) ) base_uri$`_reduri` else NULL
-
-#   if _debug:
-#     print("Creating query for language '"+query_language+"', base '"+unicode(rbase_uri)+"': "+querystring)
-
-  .Object@librdf_query <- librdf_new_query(world@librdf_world, query_language, ruri, querystring, rbase_uri)
-  .Object@result_stream <- NULL
-
-  stopifnot(!is.null(.Object@query))
+  .Object@librdf_query <- librdf_new_query(world@librdf_world, query_language, base_uri, querystring, query_uri)
 
   return(.Object)
 })
 
+#' Execute a query
+#' @description The initialize query is executed and the result is returned as a QueryResult object
+#' @param .Object a Query object
+#' @param model a Model object
+#' @return a QueryResults object
+#' @export
+setGeneric("executeQuery", function(.Object, model) {
+  standardGeneric("executeQuery")
+})
 
-#' Free memory used by a librdf model.
-#' @details After this method is called, the Model object is no longer usable and should
+#' @describeIn Query
+setMethod("executeQuery", signature("Query"), function(.Object, model) {
+  results <- librdf_query_execute(.Object@librdf_query, model@librdf_model) 
+  queryResultObj <- new("QueryResults", results)
+  return(queryResultObj)
+})
+
+#' Set limit on returned query results
+#' @param .Object a Query object
+#' @param limit the result set limit. Specify a value >= to have a limit, or a value < 0 to have no limit.
+#' @export
+setGeneric("setQueryResultLimit", function(.Object, limit) {
+  standardGeneric("setQueryResultLimit")
+})
+
+#' @describeIn Query
+setMethod("setQueryResultLimit", signature("Query"), function(.Object, limit) {
+  librdf_query_set_limit(.Object@librdf_query, as.integer(limit))
+})
+
+#' Get the query result limit
+#' @param .Object a Query object
+#' @return the query result limit. If a limit is set then the value will be >= 0. If the value is < 0, no limit is set
+#' @export
+setGeneric("getQueryResultLimit", function(.Object) {
+  standardGeneric("getQueryResultLimit")
+})
+
+#' @describeIn Query
+setMethod("getQueryResultLimit", signature("Query"), function(.Object) {
+  return(librdf_query_get_limit (.Object@librdf_query))
+})
+
+#' Free memory used by a librdf query
+#' @details After this method is called, the Query object is no longer usable and should
 #' be deleted \code{"rm(query)"} and a new object created.
-#' @param .Object a Model object
+#' @param .Object a Query object
 #' @export
 setGeneric("freeQuery", function(.Object) {
   standardGeneric("freeQuery")
 })
 
+#' @describeIn Query
 setMethod("freeQuery", signature("Query"), function(.Object) {
   librdf_free_query(.Object@librdf_query)
 })
---
+++ R/redland/R/QueryResults.R
@@ -0,0 +1,111 @@
+#
+#   This work was created by the National Center for Ecological Analysis and Synthesis.
+#
+#     Copyright 2014
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+#' QueryResult is used to retrieve query results from a Query object.
+#' @slot librdf_query A redland query object
+#' @author Peter Slaughter
+#' @rdname QueryResults-class
+#' @include redland.R
+#' @keywords classes
+#' @exportClass QueryResults
+#' @examples
+#' \dontrun{
+#' query <- new("Query", world, queryString, base_uri=NULL, query_language="sparql", query_uri=NULL)
+#' queryResult <- executeQuery(query, results)
+#' }
+setClass("QueryResults", slots = c(librdf_query_results = "_p_librdf_query_results"))
+
+#' Initialize the QueryResults object.
+#' @description The QueryResults object is initialized with the librdf query result from
+#' return value of \code{'Query.execute()'}.
+#' @details A QueryResults object is returned by the \code{Query.executeQuery()} method, so typically a user
+#' does not initialize a QueryResult object by calling \code{new("QueryResult", ...)}
+#' @param .Object the QueryResults object.
+#' @param results a librdf query result
+#' @return the QueryResults object
+#' @export
+setMethod("initialize", signature = "QueryResults", definition = function(.Object, results) {
+  .Object@librdf_query_results <- results     
+  return(.Object)
+})
+
+#' Get the next query result
+#' @description The next query result is returned
+#' @param .Object a Query object
+#' @export
+setGeneric("getNextResult", function(.Object) {
+  standardGeneric("getNextResult")
+})
+
+#' @describeIn QueryResults
+setMethod("getNextResult", signature("QueryResults"), function(.Object) {
+
+  nodeNames <- list()
+  nodeValues <- list()
+  
+  # Process the next result, storing the bound values in a list
+  if (!is.null(.Object@librdf_query_results) && librdf_query_results_finished(.Object@librdf_query_results) == 0) {
+    num_nodes <- librdf_query_results_get_bindings_count(.Object@librdf_query_results)
+    for (i in 1:num_nodes-1) {
+      binding_name <- librdf_query_results_get_binding_name(.Object@librdf_query_results, i)
+      val = librdf_query_results_get_binding_value(.Object@librdf_query_results, i)
+      # If no value returned for this binding, set to "NA"
+      if (!is.null.externalptr(val@ref)) {
+        nval <- librdf_node_to_string(val)
+      } else {
+        nval = as.character(NA)
+      }
+      nodeNames <- c(nodeNames, binding_name)
+      nodeValues <- c(nodeValues, nval)
+    }
+  } else {
+    return(NULL)
+  }
+  
+  names(nodeValues) <- nodeNames
+  nextResult <-librdf_query_results_next(.Object@librdf_query_results)
+  
+  return(nodeValues)
+})
+
+#' Free memory used by a librdf query results
+#' @details After this method is called, the QueryResults object is no longer usable and should
+#' be deleted with \code{"rm(query)"}.
+#' @param .Object a QueryResults object
+#' @export
+setGeneric("freeQueryResults", function(.Object) {
+  standardGeneric("freeQueryResults")
+})
+
+#' @describeIn QueryResults
+setMethod("freeQueryResults", signature("QueryResults"), function(.Object) {
+  # Have to free all of the nodes that were created by the query result that
+  # hold the bound node values
+  if (!is.null(.Object@librdf_query_results) && librdf_query_results_finished(.Object@librdf_query_results) == 0) {
+    num_nodes <- librdf_query_results_get_bindings_count(.Object@librdf_query_results)
+    for (i in 1:num_nodes-1) {
+      binding_name <- librdf_query_results_get_binding_name(.Object@librdf_query_results, i)
+      val = librdf_query_results_get_binding_value(.Object@librdf_query_results, i)    
+      if (!is.null.externalptr(val@ref)) {
+        librdf_free_node(val)
+      } 
+    }
+  }
+  
+  librdf_free_query_results(.Object@librdf_query_results)
+})

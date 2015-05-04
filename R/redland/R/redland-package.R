4c73d030e49f93960a966baf2d872273ae16c63f
Peter Slaughter <slaughter@nceas.ucsb.edu>
2015-05-04 16:32:14 -0700

    Additional R redland documentation

---
+++ R/redland/R/redland-package.R
@@ -0,0 +1,9 @@
+#' Create, query and write RDF graphs.
+#'
+#' @section redland
+#' @description The redland R package provides methods to create, query and write information stored in the 
+#' Resource Description Framework (RDF).
+#'
+#' @docType package
+#' @name redland
+NULL

b37214158021f06144c346b4a988b16265309d79
Peter Slaughter <slaughter@nceas.ucsb.edu>
2015-06-04 17:19:56 -0700

    include UC regents in copyright

--- R/redland/R/Model.R
+++ R/redland/R/Model.R
@@ -1,7 +1,7 @@
 #
 #   This work was created by the National Center for Ecological Analysis and Synthesis.
 #
-#     Copyright 2015
+#     Copyright 2015 Regents of the University of California
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-#
 
 #' An RDF Model object.
 #' @slot librdf_model A redland model object

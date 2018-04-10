0052f0d3cfcc75f1f90a2a0f0d31ae14915fd94d
gothub <slaughter@nceas.ucsb.edu>
2018-04-10 16:52:34 -0700

    Add getResults(), writeResults()

--- R/redland/R/Query.R
+++ R/redland/R/Query.R
@@ -23,6 +23,7 @@
 #' @details A Query is executed using the executeQuery method, which returns a QueryResults object that
 #' can be interated over the query solution sequence.
 #' @slot librdf_query A redland query object
+#' @slot librdf_world A redland world object
 #' @rdname Query-class
 #' @aliases Query
 #' @include redland.R
@@ -35,6 +36,8 @@
 #'   \item{\code{\link{executeQuery}}}{: Execute a query.}
 #'   \item{\code{\link{setQueryResultLimit}}}{: Set limit on returned query results.}
 #'   \item{\code{\link{getQueryResultLimit}}}{: Get the query result limit.}
+#'   \item{\code{\link{getResults}}}{: Return all query results.}
+#'   \item{\code{\link{writeResults}}}{: Write query results to a file.}
 #'   \item{\code{\link{freeParser}}}{: Free memory used by a librdf query.}
 #' }
 #' @seealso \code{\link{redland}}{: redland package}
@@ -62,7 +65,7 @@
 #' query <- new("Query", world, queryString, base_uri=NULL, query_language="sparql", query_uri=NULL)
 #' queryResult <- executeQuery(query, model)
 #' result <- getNextResult(queryResult)
-setClass("Query", slots = c(librdf_query = "_p_librdf_query"))
+setClass("Query", slots = c(librdf_query = "_p_librdf_query", librdf_world = "_p_librdf_world_s"))
 
 #' Initialize the Query object.
 #' @rdname Query-initialize
@@ -81,7 +84,8 @@ setMethod("initialize",
 
   stopifnot(!is.null(querystring))
   .Object@librdf_query <- librdf_new_query(world@librdf_world, query_language, base_uri, querystring, query_uri)
-
+  .Object@librdf_world <- world@librdf_world
+  
   return(.Object)
 })
 
@@ -89,7 +93,7 @@ setMethod("initialize",
 #' @description The initialize query is executed and the result is returned as a QueryResult object
 #' @rdname executeQuery
 #' @param .Object a Query object
-#' @param model a Model object
+#' @param model a Model object containing the statments to query
 #' @return a QueryResults object
 #' @export
 setGeneric("executeQuery", function(.Object, model) {
@@ -130,6 +134,106 @@ setGeneric("getQueryResultLimit", function(.Object) {
 #' @rdname getQueryResultsLimit
 setMethod("getQueryResultLimit", signature("Query"), function(.Object) {
   return(librdf_query_get_limit (.Object@librdf_query))
+})
+
+#' Return all query results
+#' @details After this method is called, the Query object is no longer usable and should
+#' be deleted \code{"rm(query)"} and a new object created.
+#' @rdname getResults
+#' @param .Object a Query object
+#' @param model a Model object
+#' @param formatName a string specifying the RDF format name. Currently the supported formats are "rdfxml" ,"turtle" and "json"
+#' @examples
+#' world <- new("World")
+#' storage <- new("Storage", world, "hashes", name="", options="hash-type='memory'")
+#' model <- new("Model", world, storage, options="")
+#' stmt <- new("Statement", world=world, 
+#'   subject="https://orcid.org/0000-0002-2192-403X",
+#'   predicate="http://www.w3.org/ns/prov#Agent",
+#'   object="slaughter", 
+#'   objectType="literal", datatype_uri="http://www.w3.org/2001/XMLSchema#string")
+#'   #objectType="literal", language="en")
+#' status <- addStatement(model, stmt)
+#' queryString <- paste("PREFIX orcid: <https://orcid.org/>",
+#'                      "PREFIX dataone: <https://cn.dataone.org/cn/v1/resolve/>",
+#'                      "PREFIX prov: <http://www.w3.org/ns/prov#>",
+#'                      "SELECT ?a ?c WHERE { ?a prov:Agent ?c . }", sep=" ")
+#' query <- new("Query", world, queryString, base_uri=NULL, query_language="sparql", query_uri=NULL)
+#' # Return all results as a string
+#' results <- getResults(query, model, "rdfxml")
+#' results <- getResults(query, model, "turtle")
+#' results <- getResults(query, model, "json")
+#' 
+#' # When the query object is no longer needed, the resources it had allocated can be freed.
+#' freeQuery(query)
+#' rm(query)
+#' @export
+#' @param ... additional parameters
+setGeneric("getResults", function(.Object, model, ...) {
+  standardGeneric("getResults")
+})
+
+#' @rdname getResults
+#' @param formatName
+setMethod("getResults", signature("Query"), function(.Object, model, formatName) {
+  queryResult <- executeQuery(.Object, model)
+  if (!is.null(queryResult@librdf_query_results) && librdf_query_results_finished(queryResult@librdf_query_results) == 0) {
+    # It appears that this redland C library function doesn't allow specifying 'formatName' as "" or NULL, so the next two
+    # arguments can never be used. Also, a text value has to be specified for the 3rd argument (mimeType) otherwise the function crashes.
+    results <- librdf_query_results_to_string2(queryResult@librdf_query_results, formatName, "application/rdf+xml", NULL, NULL);
+    return(results)
+  } else {
+    return(NULL)
+  }
+})
+
+#' Write query results to a file.
+#' @details After this method is called, the Query object is no longer usable and should
+#' be deleted \code{"rm(query)"} and a new object created.
+#' @rdname writeResults
+#' @param .Object a Query object
+#' @param model a Model object
+#' @param ... additional parameters
+#' @examples
+#' world <- new("World")
+#' storage <- new("Storage", world, "hashes", name="", options="hash-type='memory'")
+#' model <- new("Model", world, storage, options="")
+#' stmt <- new("Statement", world=world, 
+#'   subject="https://orcid.org/0000-0002-2192-403X",
+#'   predicate="http://www.w3.org/ns/prov#Agent",
+#'   object="slaughter", 
+#'   objectType="literal", datatype_uri="http://www.w3.org/2001/XMLSchema#string")
+#' status <- addStatement(model, stmt)
+#' queryString <- paste("PREFIX orcid: <https://orcid.org/>",
+#'                      "PREFIX dataone: <https://cn.dataone.org/cn/v1/resolve/>",
+#'                      "PREFIX prov: <http://www.w3.org/ns/prov#>",
+#'                      "SELECT ?a ?c WHERE { ?a prov:Agent ?c . }", sep=" ")
+#' query <- new("Query", world, queryString, base_uri=NULL, query_language="sparql", query_uri=NULL)
+#' # Return all results as a string
+#' tf <- tempfile()
+#' writeResults(query, model, file=tf, mimeType="application/x-turtle")
+#' 
+#' # When the query object is no longer needed, the resources it had allocated can be freed.
+#' freeQuery(query)
+#' rm(query)
+#' @export
+setGeneric("writeResults", function(.Object, model, ...) {
+  standardGeneric("writeResults")
+})
+
+#' @rdname writeResults
+#' @param file a string specifying the output file
+#' @param mimeType a string specifying the mimeType of the output file. Currently supported values are "application/x-turtle", "text/plain", "application/json", "text/html"
+#' @param format_uri (not currently used)
+#' @param base_uri (not currently used)
+setMethod("writeResults", signature("Query"), function(.Object, model, file, mimeType="application/x-turtle", format_uri=NULL, base_uri=NULL) {
+  queryResult <- executeQuery(.Object, model)
+  if (!is.null(queryResult@librdf_query_results) && librdf_query_results_finished(queryResult@librdf_query_results) == 0) {
+    results <- librdf_query_results_to_file2(queryResult@librdf_query_results, file, mimeType, format_uri, base_uri);
+    return(results)
+  } else {
+    return(NULL)
+  }
 })
 
 #' Free memory used by a librdf query

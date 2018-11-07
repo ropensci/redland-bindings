d03af5bf7d7a13d69118b001bc5443d278ad12a7
gothub <slaughter@nceas.ucsb.edu>
2018-11-07 11:38:36 -0800

    remove obsolete swig.sh

--- R/swig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#declare SWIG_OPTS = "-I$(srcdir) -I$(srcdir)/.."
-#swig -v -r $(SWIG_OPTS) -o Redland_wrap.c /usr/share/redland/Redland.i
-#cp NAMESPACE.in redland/NAMESPACE
-swig -v -Wall -r -module redland -outdir redland/R -o redland/src/redland.c Redland.i 
-#cat redland/R/NAMESPACE >> redland/NAMESPACE
-#rm redland/R/NAMESPACE
-cd redland/src
-R CMD SHLIB redland.c
